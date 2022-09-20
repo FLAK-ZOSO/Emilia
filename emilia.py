2dabc23601db390bc85aad471ba79b2c6fa66645
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-09-20 16:44:17 +0200

    Collect censorable words

--- emilia.py
+++ emilia.py
@@ -22,8 +22,16 @@ Emilia = Bot(
 )
 
 
+censor_data: dict[int, dict[str, dict[str, str | bool | int]]] = {}
+
+
 @Emilia.event
 async def on_ready() -> None:
+    print(f"...loading censor information from guilds directory...")
+    global censor_data
+    root, _, _ = os.walk("guilds")
+    for dir in root[1]:
+        censor_data[int(dir)] = json.load(open(f"guilds/{dir}/censor.json"))
     print(f"...({Emilia.user.name}#{Emilia.user.discriminator}) online")
 
 
@@ -42,6 +50,35 @@ async def say(interaction: Interaction, text: str, embed: bool):
     else:
         await interaction.channel.send(text)
     await interaction.response.send_message("Message sent", ephemeral=True)
+
+
+@Emilia.slash_command(description="Add a word to the soviet censor list")
+@has_permissions(administrator=True)
+async def censor(
+        interaction: Interaction, 
+        word: str, reason: str, embed: bool,
+        action: int = SlashOption(
+            name="action", description="Action to take on word match",
+            choices={"just Delete": 0, "also Kick": 1, "also Ban": 2},
+        )
+    ):
+    path = f"guilds/{interaction.guild.id}/censor.json"
+    if (not os.path.isfile(path)):
+        try:
+            file = open(path, "w")
+        except FileNotFoundError:
+            os.mkdir(f"guilds/{interaction.guild.id}")
+            file = open(path, "w")
+        file.write(r"{}")
+        file.close()
+    with open(path, "r") as file:
+        censor: dict = json.load(file)
+        censor[word] = {"reason": reason, "embed": embed, "action": action}
+        censor_data[interaction.guild.id][word] = {"reason": reason, "embed": embed, "action": action}
+    with open(path, "w") as file:
+        json.dump(censor, file, indent=4)
+    await interaction.channel.send(embed=Embed(title="Censor", description=f"Added {word} to censor list", color=Color.red()))
+    await interaction.response.send_message(f"Word {word} added to soviet censor list", ephemeral=True)
 
 
 Emilia.run(open("token.txt").read())
