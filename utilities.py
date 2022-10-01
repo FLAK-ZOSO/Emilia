f09708aadd97036026d218e1f98cfff6ee36c58e
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-10-01 16:28:42 +0200

    Constants

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 from typing import Iterable
 from nextcord.ext.commands import Bot
-from nextcord import Member
+from nextcord import Member, User
 from nextcord.message import Message
 from nextcord.embeds import Embed
 from nextcord.colour import Color
@@ -41,3 +41,15 @@ async def act_on_word_found(
                 await author.send(embed=embed_)
             await message.delete()
             await author.ban(reason=reason)
+
+
+def started_spying(user: Member | User) -> Embed:
+    return Embed(
+        title = "SPY", 
+        description = f"I started spying on {user.mention}.", 
+        color = Color.red()
+    )
+
+
+papocchio_url = "https://static.miraheze.org/nonciclopediawiki/c/cd/Papocchio_2000x2000.png"
+stop_spying_string = "Stop spying on "
